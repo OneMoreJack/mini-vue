05521688a30a0c7ac819504a307f4491369b9c6d
JackLiR8 <jiakeli1024@gmail.com>
2022-05-01 22:04:46 +0800

    feat(effect): add `stop`

--- src/reactivity/__tests__/effect.spec.ts
+++ src/reactivity/__tests__/effect.spec.ts
@@ -7,7 +7,7 @@
  * @FilePath: /mini-vue/src/reactivity/__tests__/index.spec.ts
  */
 import { reactive } from '../reactive'
-import { effect } from '../effect'
+import { effect, stop } from '../effect'
 
 describe('effect', () => {
   it('happy path', () => {
@@ -63,5 +63,32 @@ describe('effect', () => {
     run()
     // should have run
     expect(dummy).toBe(2)
+  })
+
+  it('stop', () => {
+    let dummy
+    const obj = reactive({ prop: 1 })
+    const runner = effect(() => {
+      dummy = obj.prop
+    })
+    obj.prop = 2
+    expect(dummy).toBe(2)
+    stop(runner)
+    obj.prop = 3
+    expect(dummy).toBe(2)
+
+    // stopped effect should still be manually callable
+    runner()
+    expect(dummy).toBe(3)
+  })
+
+  it('events: onStop', () => {
+    const onStop = jest.fn()
+    const runner = effect(() => {}, {
+      onStop
+    })
+
+    stop(runner)
+    expect(onStop).toHaveBeenCalled()
   })
 })
--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,6 +1,11 @@
+import { extend } from "../shared"
+
 class ReactiveEffect {
   private _fn: any
   public scheduler
+  deps: any[] = []
+  active = true
+  onStop?: () => void
 
   constructor(fn, scheduler?) {
     this._fn = fn
@@ -11,6 +16,22 @@ class ReactiveEffect {
     activeEffect = this
     return this._fn()
   }
+
+  stop() {
+    if (this.active) {
+      cleanupEffect(this);
+      if (this.onStop) {
+        this.onStop()
+      }
+      this.active = false;
+    }
+  }
+}
+
+function cleanupEffect(effect) {
+  effect.deps.forEach(dep => {
+    dep.delete(effect)
+  });
 }
 
 const targetMap = new Map()
@@ -27,7 +48,10 @@ export function track(target, key) {
     depMap.set(key, dep)
   }
 
+  if (!activeEffect) return
+
   dep.add(activeEffect)
+  activeEffect.deps.push(dep)
 }
 
 export function trigger(target, key) {
@@ -47,6 +71,16 @@ export function trigger(target, key) {
 let activeEffect;
 export function effect(fn, options: any = {}) {
   const _effect = new ReactiveEffect(fn, options.scheduler)
+  extend(_effect, options)
+  
   _effect.run()
-  return _effect.run.bind(_effect)
+
+  const runner: any = _effect.run.bind(_effect)
+  runner.effect = _effect
+
+  return runner
+}
+
+export function stop(runner) {
+  runner.effect.stop()
 }
